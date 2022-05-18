cf9fecc51e02e98cd61f449cee3ca57793b46043
Omar Mohamed <om644123@gmail.com>
2022-05-18 02:25:19 +0200

    Add project files.

---
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to Our Coffee Shop");
+        }
+    }
+}
