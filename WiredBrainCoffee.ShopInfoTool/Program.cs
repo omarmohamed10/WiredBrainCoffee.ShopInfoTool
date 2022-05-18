b13155f5c522cf417a3244958d1237956f9354fb
Omar Mohamed <om644123@gmail.com>
2022-05-18 02:44:18 +0200

    Add Help Command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
@@ -10,7 +7,33 @@ namespace WiredBrainCoffee.ShopInfoTool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello to Our Coffee Shop");
+            Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+              "write 'quit' to exit application");
+
+            var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
+                var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach (var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
         }
     }
 }
